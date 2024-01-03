8b88589715ff676b230d28e46098755d9a7934cb
MiguelGuevara1071 <miguelguevara1071@gmail.com>
2024-01-02 19:10:14 -0500

    ajuste url db

--- src/db.js
+++ src/db.js
@@ -6,10 +6,10 @@ import { USERNAME, PASSWORD, DB } from './config.js';
 */
 export const conectDB = async () => {
     // URl en la Nube Atlas Mongo db
-    // const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.keqxeqo.mongodb.net/${DB}?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.keqxeqo.mongodb.net/${DB}?retryWrites=true&w=majority`
 
     // Url en local require tener inastalado Mongo DB en local
-    const URL = `mongodb://localhost:27017/${DB}`
+    // const URL = `mongodb://localhost:27017/${DB}`
     try{
         await mongoose.connect(URL)
         console.log(">>> DB is conected")
